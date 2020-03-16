acacc385710b754094b712591a852a0335aaee24
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-03-16 11:52:22 +0800

    add

--- Assets/_02CreateComponentCode/Editor/CreateComponentCode.cs
+++ Assets/_02CreateComponentCode/Editor/CreateComponentCode.cs
@@ -1,12 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public static class CreateComponentCode
 {
+    public static string ScriptName(string file)
+    {
+        return file + "Code";
+    }
+
+    public static string ObjectName(string scriptName)
+    {
+        if (scriptName.Length > 4)
+        {
+            return scriptName.Substring(0, scriptName.Length - 4);
+        }
+
+        return scriptName;
+    }
+
     [MenuItem("GameObject/@EditorExtension-Create Code", false, 0)]
     private static void Create()
     {
@@ -26,7 +43,7 @@ public static class CreateComponentCode
             Directory.CreateDirectory(scriptsFolder);
         }
 
-        string className = gameobject.name + "Code";
+        string className = ScriptName(gameobject.name);
         var scriptFile = scriptsFolder + className + ".cs";
         using (var sw = File.CreateText(scriptFile))
         {
@@ -35,8 +52,77 @@ public static class CreateComponentCode
             sw.Close();
         }
 
+        //确保难以出现两个
+        //GameObjectUtility.RemoveMonoBehavioursWithMissingScript(gameobject);
+
+        //因为脚本是动态创建的 所以需要刷新才会出现
+        //但是刷新会重新编译 清空缓存   
+        EditorPrefs.SetString("GENERATE_CLASS_NAME", className);
         AssetDatabase.Refresh();
 
         Debug.Log("Create Code");
+    }
+
+    //DidReloadScripts 编译完成之后callback 事件
+    [DidReloadScripts]
+    private static void AddComponentToGameObject()
+    {
+        //Debug.Log("DidReloadScripts");
+        var generateClassName = EditorPrefs.GetString("GENERATE_CLASS_NAME");
+        //避免改其他的时候调用到
+        EditorPrefs.SetString("GENERATE_CLASS_NAME", "");
+        //Debug.Log(generateClassName);
+
+        if (string.IsNullOrWhiteSpace(generateClassName))
+        {
+            //Debug.Log("class name is null , breaking!");
+            return;
+        }
+
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        var assembly = assemblies.First(temp => temp.GetName().Name == "Assembly-CSharp");
+        if (assembly != null)
+        {
+            var cls = assembly.GetType(generateClassName);
+            if (cls != null)
+            {
+                //因为会清空缓存 所以没有办法缓存GameObject
+                var obj = GameObject.Find(ObjectName(generateClassName));
+                var instance = obj.GetComponent(cls);
+                if (instance == null)
+                    instance = obj.AddComponent(cls);
+                List<string> cachePath = new List<string>();
+                SearchUIBind(obj.transform, cachePath);
+                foreach (var item in cachePath)
+                {
+                    Debug.Log(item);
+                }
+            }
+        }
+    }
+
+
+    public static void SearchUIBind(Transform nowObj, List<string> cachePath, string basePath = null)
+    {
+        var bind = nowObj.GetComponent<UIBind>();
+        if (bind)
+        {
+            if (basePath == null)
+            {
+                cachePath.Add("");
+            }
+            else
+            {
+                cachePath.Add(basePath);
+            }
+        }
+
+
+        foreach (Transform item in nowObj)
+        {
+            //如果 find(/ + path) 则为 绝对路径查找  从最外层可以查找
+            //应该杜绝
+            SearchUIBind(item, cachePath, (basePath == null ? "" : basePath + "/") + item.name);
+        }
     }
 }
