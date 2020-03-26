bb956d2f287084eb66c094136ead982692b37195
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-03-26 22:38:41 +0800

    add

---
+++ Assets/_04TodoList/Editor/MainWindow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MainWindow : EditorWindow
+{
+    private bool isShow;
+
+    [MenuItem("TodoList/MainWindow %#t")]
+    public static void Open()
+    {
+        var window = GetWindow<MainWindow>();
+        if (window.isShow)
+        {
+            window.Show();
+            window.isShow = true;
+        }
+        else
+        {
+            window.Close();
+            window.isShow = false;
+        }
+    }
+}
