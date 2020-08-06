c9f5d7ffffcc1aad7d767a22acdd98dec63f2729
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-08-07 00:06:49 +0800

    add

--- Assets/_04ToDoList/Editor/FrameWork/ViewGUI/ToDoListNoteEditorView.cs
+++ Assets/_04ToDoList/Editor/FrameWork/ViewGUI/ToDoListNoteEditorView.cs
@@ -1,37 +1,44 @@
-﻿using _04ToDoList.Editor.FrameWork.DataBinding;
+﻿using System;
+using _04ToDoList.Editor.FrameWork.DataBinding;
 using _04ToDoList.Editor.FrameWork.Drawer;
 using _04ToDoList.Editor.FrameWork.Layout;
+using UnityEditor;
 using UnityEngine;
 
 namespace _04ToDoList.Editor.FrameWork.ViewGUI
 {
     public class ToDoListNoteEditorView : VerticalLayout
     {
-        public ToDoListNoteEditorView()
+        private TextAreaView textEditor;
+
+        public ToDoListNoteEditorView(Action saveAction)
         {
             Style = "box";
 
             new LabelView("笔记编辑器")
+                .TextMiddleCenter()
                 .TheFontStyle(FontStyle.Bold)
                 .FontSize(40)
                 .AddTo(this);
 
-            var textEditor = new TextAreaView(string.Empty).AddTo(this);
+            textEditor = new TextAreaView(string.Empty)
+                .ExpandHeight(true)
+                .AddTo(this);
 
             new ButtonView("保存", () =>
                 {
-                    EnqueueCmd(() =>
-                    {
-                        var model = ToDoListCls.ModelData;
-                        model.notes.Add(new ToDoNote(textEditor.Content.Val));
-                        model.Save();
-                        ParentRemoveThis();
-                    });
-                    
+                    EditorGUI.FocusTextInControl(null);
+                    var model = ToDoListCls.ModelData;
+                    model.notes.Add(new ToDoNote(textEditor.Content.Val));
+                    model.Save();
+                    saveAction();
                 }, true)
                 .AddTo(this);
         }
 
-
+        protected override void OnShow()
+        {
+            textEditor.Content.Val = string.Empty;
+        }
     }
 }
--- Assets/_04ToDoList/Editor/FrameWork/ViewGUI/ToDoListNoteView.cs
+++ Assets/_04ToDoList/Editor/FrameWork/ViewGUI/ToDoListNoteView.cs
@@ -1,4 +1,5 @@
-﻿using _04ToDoList.Editor.FrameWork.Drawer;
+﻿using _04ToDoList.Editor.FrameWork.DataBinding;
+using _04ToDoList.Editor.FrameWork.Drawer;
 using _04ToDoList.Editor.FrameWork.Layout;
 using UnityEngine;
 
@@ -6,27 +7,62 @@ namespace _04ToDoList.Editor.FrameWork.ViewGUI
 {
     public class ToDoListNoteView : VerticalLayout
     {
-        private bool needAdd;
+        private LabelView titleLabelView;
+        private ButtonView createButtonView;
+        private ToDoListNoteEditorView editorView;
+
+
+        private bool isDirty;
 
         public ToDoListNoteView()
         {
             Style = "box";
 
-            new LabelView("欢迎来到笔记界面").AddTo(this);
-            new ButtonView("创建笔记", () => { needAdd = true; }, true).AddTo(this);
+            titleLabelView = new LabelView("欢迎来到笔记界面")
+                .TextMiddleCenter()
+                .TheFontStyle(FontStyle.Bold)
+                .FontSize(40).AddTo(this);
+            createButtonView = new ButtonView("创建笔记", CreateNewEditor, true).AddTo(this);
+            editorView = new ToDoListNoteEditorView(SaveAction).AddTo(this);
 
-            //TODO:show note
+            editorView.Hide();
+            isDirty = true;
         }
 
         protected override void OnRefresh()
         {
-            if (!needAdd)
+            if (!isDirty)
             {
                 return;
             }
 
-            needAdd = false;
-            new ToDoListNoteEditorView().AddTo(this);
+            isDirty = false;
+            var notes = ToDoListCls.ModelData.notes;
+
+
+            foreach (var item in notes)
+            {
+                new LabelView(item.content).AddTo(this);
+            }
+        }
+
+        public void CreateNewEditor()
+        {
+            titleLabelView.Hide();
+            createButtonView.Hide();
+            editorView.Show();
+            EnqueueCmd(() =>
+            {
+                
+            });
+        }
+
+        public void SaveAction()
+        {
+            titleLabelView.Show();
+            createButtonView.Show();
+            editorView.Hide();
+            isDirty = true;
         }
     }
 }
