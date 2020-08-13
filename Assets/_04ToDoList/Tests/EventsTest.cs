6dbf43a222766b9ff581bad44c98f1c8846c255e
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-08-13 23:58:33 +0800

    add

---
+++ Assets/_04ToDoList/Tests/EventsTest.cs
@@ -0,0 +1,35 @@
+﻿using _04ToDoList.Editor.FrameWork.DataBinding;
+using _04ToDoList.Util;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class EventsTest
+    {
+        public enum TestEvents
+        {
+            A,
+            B,
+            C,
+            D,
+        }
+
+        [Test]
+        public void TestEvent01()
+        {
+            EventDispatcher.Register(TestEvents.A, () =>
+            {
+                Debug.Log(1);
+            });
+
+            EventDispatcher.Send(TestEvents.A);
+
+            EventDispatcher.RemoveAll(TestEvents.A);
+
+            EventDispatcher.Send(TestEvents.A);
+
+        }
+    }
+}
--- Assets/_04ToDoList/Util/EventDispatcher.cs
+++ Assets/_04ToDoList/Util/EventDispatcher.cs
@@ -8,10 +8,10 @@ namespace _04ToDoList.Util
     {
         private static Dictionary<int, Action> registeredEvents = new Dictionary<int, Action>();
 
-        public static void Register<T>(Action _onEvent) where T : IConvertible
+        public static void Register<T>(T key, Action _onEvent) where T : IConvertible
         {
-            //delegate 只能这样写
-            int intKey = default(T).ToInt32(null);
+            //delegate 只能这样写  因为可能是值类型
+            int intKey = key.ToInt32(null);
             if (!registeredEvents.ContainsKey(intKey))
             {
                 Action act = _onEvent;
@@ -20,29 +20,6 @@ namespace _04ToDoList.Util
             else
             {
                 registeredEvents[intKey] += _onEvent;
-            }
-        }
-
-        public static void Register<T>(T type, Action _onEvent) where T : IConvertible
-        {
-            int intKey = type.ToInt32(null);
-            if (!registeredEvents.ContainsKey(intKey))
-            {
-                Action act = _onEvent;
-                registeredEvents.Add(intKey, act);
-            }
-            else
-            {
-                registeredEvents[intKey] += _onEvent;
-            }
-        }
-
-        public static void Remove<T>(Action _onEvent) where T : IConvertible
-        {
-            int intKey = default(T).ToInt32(null);
-            if (registeredEvents.ContainsKey(intKey))
-            {
-                registeredEvents[intKey] -= _onEvent;
             }
         }
 
@@ -54,4 +31,25 @@ namespace _04ToDoList.Util
                 registeredEvents[intKey] -= _onEvent;
             }
         }
+
+        public static void RemoveAll<T>(T key) where T : IConvertible
+        {
+            int intKey = key.ToInt32(null);
+            if (registeredEvents.TryGetValue(intKey, out var acts))
+            {
+                //也可以这样获取全部的方法
+                //registeredEvents[intKey].GetInvocationList()
+                registeredEvents[intKey]-= acts;
+            }
+        }
+
+        public static void Send<T>(T key) where T : IConvertible
+        {
+            int intKey = key.ToInt32(null);
+            if (registeredEvents.TryGetValue(intKey, out var acts))
+            {
+                acts?.Invoke();
+            }
+        }
     }
+}
