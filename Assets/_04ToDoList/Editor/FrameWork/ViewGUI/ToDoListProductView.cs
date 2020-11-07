a6d64212a4516ad92e3d39ef8dcf6617568ff2da
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-11-07 23:49:59 +0800

    add

--- Assets/_04ToDoList/Editor/FrameWork/ViewGUI/ToDoListProductView.cs
+++ Assets/_04ToDoList/Editor/FrameWork/ViewGUI/ToDoListProductView.cs
@@ -1,4 +1,5 @@
 ﻿using _04ToDoList.Editor.FrameWork.Drawer;
+using _04ToDoList.Editor.FrameWork.Layout;
 using _04ToDoList.Editor.FrameWork.ViewController;
 using _04ToDoList.Editor.FrameWork.Window;
 using UnityEngine;
@@ -11,6 +12,15 @@ namespace _04ToDoList.Editor.FrameWork.ViewGUI
 		{
 			new ButtonView("创建产品", OpenProductEditorWindow, true).AddTo(this);
 			new LabelView("这个是产品页面").AddTo(this).TheFontStyle(FontStyle.Bold).FontSize(30);
+
+			foreach (var product in ToDoDataManager.Data.productList)
+			{
+				var hor =  new HorizontalLayout().AddTo(this);
+				
+				new LabelView(product.name).FontSize(20).TheFontStyle(FontStyle.Bold).Height(20).Width(60).AddTo(hor);
+				
+				new LabelView(product.description).FontSize(12).TheFontStyle(FontStyle.Bold).Height(20).AddTo(hor);
+			}
 		}
 
 		public void OpenProductEditorWindow()
